be427b4111aa016b2ff501d28cde99801253eba7
Lirovsky <pablolangerprofissional@gmail.com>
2025-04-04 14:07:18 -0300

    feat:improving accessibility

--- js/script.js
+++ js/script.js
@@ -1,10 +1,77 @@
 function showSidebar() {
-  const sidebar = document.querySelector(".navbar ul");
+  const sidebar = document.querySelector(".mobile-menu ul");
   if (sidebar.classList.contains("open")) {
     sidebar.classList.remove("open");
-    document.querySelector(".menuIcon img").src = "../assets/images/icons/menu.svg";
+    document.querySelector(".mobileMenuIcon img").src = "./assets/images/icons/menu.svg";
+    document.querySelector(".mobile-menu").classList.toggle("display");
   } else {
     sidebar.classList.add("open");
-    document.querySelector(".menuIcon img").src = "../assets/images/icons/close.svg";
+    document.querySelector(".mobileMenuIcon img").src = "./assets/images/icons/close.svg";
+    document.querySelector(".mobile-menu").classList.toggle("display");
+    document.querySelector(".mobile-menu ul").style.left = "-100%";
+
+    setTimeout(() => {
+      document.querySelector(".mobile-menu ul").style.left = "0";
+    }, "0");
   }
 }
+function showSidebar2() {
+  const sidebar = document.querySelector(".mobile-menu ul");
+  if (sidebar.classList.contains("open")) {
+    sidebar.classList.remove("open");
+    document.querySelector(".mobileMenuIcon img").src = "../assets/images/icons/menu.svg";
+    document.querySelector(".mobile-menu").classList.toggle("display");
+  } else {
+    sidebar.classList.add("open");
+    document.querySelector(".mobileMenuIcon img").src = "../assets/images/icons/close.svg";
+    document.querySelector(".mobile-menu").classList.toggle("display");
+    document.querySelector(".mobile-menu ul").style.left = "-100%";
+
+    setTimeout(() => {
+      document.querySelector(".mobile-menu ul").style.left = "0";
+    }, "0");
+  }
+}
+function showSidebar3() {
+  const sidebar = document.querySelector(".mobile-menu ul");
+  if (sidebar.classList.contains("open")) {
+    sidebar.classList.remove("open");
+    document.querySelector(".mobileMenuIcon img").src = "../../assets/images/icons/menu.svg";
+    document.querySelector(".mobile-menu").classList.toggle("display");
+  } else {
+    sidebar.classList.add("open");
+    document.querySelector(".mobileMenuIcon img").src = "../../assets/images/icons/close.svg";
+    document.querySelector(".mobile-menu").classList.toggle("display");
+    document.querySelector(".mobile-menu ul").style.left = "-100%";
+
+    setTimeout(() => {
+      document.querySelector(".mobile-menu ul").style.left = "0";
+    }, "0");
+  }
+}
+
+// function showSidebar2() {
+//   const sidebar = document.querySelector(".navbar ul");
+//   if (sidebar.classList.contains("open")) {
+//     sidebar.classList.remove("open");
+//     document.querySelector(".menuIcon img").src = "../assets/images/icons/menu.svg";
+//   } else {
+//     sidebar.classList.add("open");
+//     document.querySelector(".menuIcon img").src = "../assets/images/icons/close.svg";
+//   }
+// }
+
+// function showSidebar3() {
+//   const sidebar = document.querySelector(".navbar ul");
+//   if (sidebar.classList.contains("open")) {
+//     sidebar.classList.remove("open");
+//     document.querySelector(".menuIcon img").src = "../../assets/images/icons/menu.svg";
+//   } else {
+//     sidebar.classList.add("open");
+//     document.querySelector(".menuIcon img").src = "../../assets/images/icons/close.svg";
+//   }
+// }
+
+document.getElementById("focusButton").addEventListener("click", () => {
+  document.getElementById("buttonFocusMobile").focus();
+});
